b95f593fa007b3bd5274a141296adefc6c599114
sohak22 <sohakn22@gmail.com>
2023-03-03 17:03:39 -0500

    adding jest files

---
+++ client/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    preset: 'ts-jest',
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        "^.+\\.(js|jsx)$": "babel-jest",
+    },
+    moduleFileExtensions: ['js', 'jsx'],
+    moduleDirectories: ['node_modules', 'bower_components', 'shared'],
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+        "\\.(scss|sass|css)$": "identity-obj-proxy",
+        "@fontsource/oswald": "identity-obj-proxy",
+        "@fontsource/inter": "identity-obj-proxy",
+        "@mui/material": "identity-obj-proxy",
+        "@faker-js/faker": "identity-obj-proxy",
+        "@testing-library/user-event": "identity-obj-proxy",
+
+
+    },
+};
