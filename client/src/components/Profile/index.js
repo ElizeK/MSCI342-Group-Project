da8b02b56f2607ecf6a0e5747cc9d221a337fada
Swetaa Suresh <swetaa232002@gmail.com>
2023-03-14 13:40:07 -0400

    updated button app bar

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,76 +1,192 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
 import { Typography } from "@material-ui/core";
 import history from "../Navigation/history";
+import "@fontsource/oswald";
+import "@fontsource/inter";
+import { Grid, Toolbar, Button, Paper, FormControl, InputLabel, Select, MenuItem, TextField, Box } from '@mui/material';
+import { LastPageOutlined } from '@material-ui/icons';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
 
-import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
-import { green } from '@material-ui/core/colors';
-import Paper from "@material-ui/core/Paper";
+// This theme sets the background color for when you scroll behind the screen
+const theme = createTheme({
+    palette: {
+        type: 'dark',
+        background: {
+            default: "#1b1b1b"
+        },
+        primary: {
+            main: "#FFFFFF",
+        },
+        secondary: {
+            main: "#1b1b1b",
+        },
+    },
+});
 
-import { deepOrange, deepPurple } from '@mui/material/colors';
+const opacityValue = 1;
 
+const useStyles = makeStyles((theme) => ({
+    body: {
+        backgroundColor: "#1b1b1b",
+        overflow: "hidden",
+        color: "#1b1b1b"
+    },
+    backgroundColor: {
+        backgroundColor: "#1b1b1b"
+    },
+    paper: {
+        overflow: "hidden",
+    },
+    message: {
+        opacity: opacityValue,
+        maxWidth: 250,
+        paddingBottom: theme.spacing(2),
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 200,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    textField: {
+        "& .MuiInputBase-root": {
+            color: 'white'
+        },
+        "& .MuiOutlinedInput-root": {
+            "& > fieldset": { borderColor: "white" },
+        },
+        "& .MuiInputLabel-root": { color: 'white' },
+        "& .MuiOutlinedInputLabel-root": { color: 'white' },
+        "& .MuiOutlinedInput-root:hover": {
+            "& > fieldset": {
+                borderColor: "white"
+            }
+        },
+        "& .MuiOutlinedInput-root.Mui-focused": {
+            "& > fieldset": {
+                borderColor: "white"
+            }
+        }
+    },
+    select: {
+        '&:before': {
+            borderColor: 'white',
+        },
+        '&:after': {
+            borderColor: 'white',
+        },
+        '&:not(.Mui-disabled):hover::before': {
+            borderColor: 'white',
+        },
+        '& .MuiOutlinedInput-notchedOutline': {
+            border: "1px solid white !important"
+        },
+        '& .MuiOutlinedInput-notchedOutline.Mui-focused': {
+            borderColor: "white !important"
+        },
+        '& .MuiSvgIcon-root': {
+            fill: "white !important",
+        }
+    },
+    heading: {
+        color: "white",
+        fontFamily: "Oswald",
+        fontSize: 55
+    },
+    subHeading: {
+        color: "white",
+        fontFamily: "Inter",
+        fontSize: 22,
+        fontStyle: "normal",
+        fontWeight: 300
 
+    },
+    navbarItem: {
+        fontFamily: 'Inter',
+        fontStyle: "normal",
+        fontWeight: 400,
+        fontSize: 16,
+        cursor: "pointer"
+    },
+    inputLabelRoot: {
+        color: "white",
+    }
+}));
 
 
 
 const ButtonAppBar = () => {
+    const classes = useStyles();
     return (
-        <Toolbar>
-            <Typography style={{ marginRight: 10 }}></Typography>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                News App
-            </Typography>
-            <Typography style={{ marginRight: 50 }}></Typography>
-            <Button
-                color="inherit"
-                style={{ cursor: "pointer" }}
-                onClick={() => history.push('/')}
-            >
-                Home
-            </Button>
-            <Typography style={{ marginRight: 50 }}></Typography>
-            <Button
-                color="inherit"
-                style={{ cursor: "pointer" }}
-                onClick={() => history.push('/Search')}
-            >
-                Search
-            </Button>
-            <Typography style={{ marginRight: 50 }}></Typography>
-            <Button
-                color="inherit"
-                style={{ cursor: "pointer" }}
-                onClick={() => history.push('/ThinkPiece')}
-            >
-                ThinkPiece
-            </Button>
-            <Typography style={{ marginRight: 50 }}></Typography>
-            <Button
-                color="inherit"
-                style={{ cursor: "pointer" }}
-                onClick={() => history.push('/Profile')}
-            >
-                Profile
-            </Button>
-            <Typography style={{ marginRight: 50 }}></Typography>
+        <div>
+            <Toolbar>
+                <Typography style={{ marginRight: 10 }}></Typography>
+                <Button
+                    color="inherit"
+                    onClick={() => history.push('/Landing')}
+                >
+                    <Typography className={classes.navbarItem} variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        Pulse News
+                    </Typography>
+                </Button>
+                <Typography style={{ marginRight: 50 }}></Typography>
+                <Button
+                    color="inherit"
+                    onClick={() => history.push('/Home')}
+                >
+                    <Typography className={classes.navbarItem}>Home</Typography>
+                </Button>
+                <Typography style={{ marginRight: 50 }}></Typography>
+                <Button
+                    color="inherit"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => history.push('/Search')}
+                >
+                    <Typography className={classes.navbarItem}>Search</Typography>
+                </Button>
+                <Typography style={{ marginRight: 50 }}></Typography>
+                <Button
+                    color="inherit"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => history.push('/ThinkPiece')}
+                >
+                    <Typography className={classes.navbarItem}>Thinkpiece</Typography>
+                </Button>
+                <Typography style={{ marginRight: 50 }}></Typography>
+                <Button
+                    color="inherit"
+                    style={{ cursor: "pointer" }}
+                    onClick={() => history.push('/Profile')}
+                >
+                    <Typography className={classes.navbarItem}>Profile</Typography>
+                </Button>
 
-        </Toolbar>
+            </Toolbar>
+        </div>
+
     );
 }
 
 
 const Profile = () => {
+    const classes = useStyles();
 
 
     return (
         <div>
             <ButtonAppBar
-                backgroundColor="secondary"
+                backgroundColor="primary"
             ></ButtonAppBar>
             <Grid
                 container
@@ -94,90 +210,23 @@ const Profile = () => {
 
 const serverURL = "http://ec2-18-216-101-119.us-east-2.compute.amazonaws.com:3054"; //enable for deployed mode; Change PORT to the port number given to you;
 
-const opacityValue = 0.7;
 
-const theme = createTheme({
-    palette: {
-        type: 'dark',
-        background: {
-            default: "#b552f7"
-        },
-        primary: {
-            main: "#52F7B4",
-        },
-        secondary: {
-            main: "#b552f7",
-        },
-    },
-});
-const styles = theme => ({
-    root: {
-        body: {
-            backgroundColor: "#AFE1AF",
-            opacity: opacityValue,
-            overflow: "hidden",
-            color: green[400],
-            '&$checked': {
-                color: green[600],
-            },
-        },
-    },
-    paper: {
-        overflow: "hidden",
-    },
-    message: {
-        opacity: opacityValue,
-        maxWidth: 250,
-        paddingBottom: theme.spacing(2),
-    },
-    formControl: {
-        margin: theme.spacing(1),
-        minWidth: 200,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing(2),
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
 
-});
-class Profiles extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userID: 1,
-            mode: 0
-        }
-    };
-    componentDidMount() {
-        //this.loadUserSettings();
-    }
+const Profiles = () => {
 
-    render() {
-        const { classes } = this.props;
+    return (
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <Paper>
+                <Profile />
+            </Paper>
 
-        return (
-            <MuiThemeProvider theme={theme}>
-                <div className={classes.root}>
-                    <CssBaseline />
-                    <Paper
-                        className={classes.paper}
-                    >
-                        <Profile />
-                    </Paper>
 
-                </div>
-
-            </MuiThemeProvider>
-        );
-    }
-}
-
-Profiles.propTypes = {
-    classes: PropTypes.object.isRequired
+        </MuiThemeProvider>
+    );
 };
 
-export default withStyles(styles)(Profiles);
+
+
+
+export default Profiles;
